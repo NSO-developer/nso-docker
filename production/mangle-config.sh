b67aa5070f6149c5e89a892a43b19fcd67dc56bf
Kristian Larsson <kristian@spritelink.net>
2019-09-24 08:40:49 +0200

    Switch to protocol default ports & enable
    This switches the northbound interface ports to their protocol defaults, like
    NETCONF over SSH to 830 and SSH CLI to 22. The default in the configuration file
    are probably to avoid low ports so that NCS can be started without root. We are
    however running in a docker container and so we are root. Listening to the
    protocol default ports thus makes more sense.
    
    We also enable SSH CLI (22), NETCONF over SSH (830) and NETCONF call-home per
    default. The HTTP API is not enabled per default but requires an environment
    variable to be passed in. We should have a similar one for HTTPS but it also
    requires SSL certificates to be setup so we need to deal with that first.

--- production/mangle-config.sh
+++ production/mangle-config.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
-mv /etc/ncs/ncs.conf /etc/ncs/ncs.conf.orig
-xmlstarlet edit -N x=http://tail-f.com/yang/tailf-ncs-config \
-	--update "/x:ncs-config/x:netconf-north-bound/x:transport/x:ssh/x:enabled" --value "true" \
-	/etc/ncs/ncs.conf.orig > /etc/ncs/ncs.conf
+CONF_FILE=/etc/ncs/ncs.conf
+
+# update ports for various protocols for which the default value in ncs.conf is
+# different from the protocols default port (to allow starting ncs without root)
+# NETCONF call-home is already on its default 4334 since that's above 1024
+xmlstarlet --inplace edit -N x=http://tail-f.com/yang/tailf-ncs-config \
+           --update "/x:ncs-config/x:cli/x:ssh/x:port" --value "22" \
+           --update "/x:ncs-config/x:webui/x:transport/x:tcp/x:port" --value "80" \
+           --update "/x:ncs-config/x:webui/x:transport/x:ssl/x:port" --value "443" \
+           --update "/x:ncs-config/x:netconf-north-bound/x:transport/x:ssh/x:port" --value "830" \
+           $CONF_FILE
+
+# enable SSH CLI, NETCONF over SSH northbound and NETCONF call-home
+xmlstarlet --inplace edit -N x=http://tail-f.com/yang/tailf-ncs-config \
+           --update "/x:ncs-config/x:cli/x:ssh/x:enabled" --value "true" \
+           --update "/x:ncs-config/x:netconf-north-bound/x:transport/x:ssh/x:enabled" --value "true" \
+           --update "/x:ncs-config/x:netconf-call-home/x:enabled" --value "true" \
+           $CONF_FILE
+
+# conditionally enable webUI with no SSL on port 80
+if [ "$HTTP_ENABLE" == "true" ]; then
+    xmlstarlet --inplace edit -N x=http://tail-f.com/yang/tailf-ncs-config \
+               --update "/x:ncs-config/x:webui/x:transport/x:tcp/x:enabled" --value "true" \
+               $CONF_FILE
+fi
+#         --update "/x:ncs-config/x:webui/x:transport/x:ssl/x:enabled" --value "true" \
