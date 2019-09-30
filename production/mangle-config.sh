dcc30ce64cb8c5bcb7c14e428591d413bf60194d
Kristian Larsson <kristian@spritelink.net>
2019-09-30 08:53:41 +0200

    Switch to single quotes
    Made a mistake on the SSL config where I had double quotes which made bash
    replace the variable name, but NCS is supposed to replace it. Must use single
    quote to prevent bash from touching it. Switching to single quotes everywhere
    for consistency...

--- production/mangle-config.sh
+++ production/mangle-config.sh
@@ -5,45 +5,45 @@ CONF_FILE=/etc/ncs/ncs.conf
 # switch to local auth per default, allow to override through environment variable PAM
 if [ "$PAM" != "true" ]; then
     xmlstarlet edit --inplace -N x=http://tail-f.com/yang/tailf-ncs-config \
-               --update "/x:ncs-config/x:aaa/x:pam/x:enabled" --value "false" \
-               --update "/x:ncs-config/x:aaa/x:local-authentication/x:enabled" --value "true" \
+               --update '/x:ncs-config/x:aaa/x:pam/x:enabled' --value 'false' \
+               --update '/x:ncs-config/x:aaa/x:local-authentication/x:enabled' --value 'true' \
                $CONF_FILE
 fi
 
 # change SSH key dir
 xmlstarlet edit --inplace -N x=http://tail-f.com/yang/tailf-ncs-config \
-           --update "/x:ncs-config/x:aaa/x:ssh-server-key-dir" --value '${NCS_RUN_DIR}/ssh' \
+           --update '/x:ncs-config/x:aaa/x:ssh-server-key-dir' --value '${NCS_RUN_DIR}/ssh' \
            $CONF_FILE
 
 # update ports for various protocols for which the default value in ncs.conf is
 # different from the protocols default port (to allow starting ncs without root)
 # NETCONF call-home is already on its default 4334 since that's above 1024
 xmlstarlet edit --inplace -N x=http://tail-f.com/yang/tailf-ncs-config \
-           --update "/x:ncs-config/x:cli/x:ssh/x:port" --value "22" \
-           --update "/x:ncs-config/x:webui/x:transport/x:tcp/x:port" --value "80" \
-           --update "/x:ncs-config/x:webui/x:transport/x:ssl/x:port" --value "443" \
-           --update "/x:ncs-config/x:netconf-north-bound/x:transport/x:ssh/x:port" --value "830" \
+           --update '/x:ncs-config/x:cli/x:ssh/x:port' --value '22' \
+           --update '/x:ncs-config/x:webui/x:transport/x:tcp/x:port' --value '80' \
+           --update '/x:ncs-config/x:webui/x:transport/x:ssl/x:port' --value '443' \
+           --update '/x:ncs-config/x:netconf-north-bound/x:transport/x:ssh/x:port' --value '830' \
            $CONF_FILE
 
 # enable SSH CLI, NETCONF over SSH northbound and NETCONF call-home
 xmlstarlet edit --inplace -N x=http://tail-f.com/yang/tailf-ncs-config \
-           --update "/x:ncs-config/x:cli/x:ssh/x:enabled" --value "true" \
-           --update "/x:ncs-config/x:netconf-north-bound/x:transport/x:ssh/x:enabled" --value "true" \
-           --update "/x:ncs-config/x:netconf-call-home/x:enabled" --value "true" \
+           --update '/x:ncs-config/x:cli/x:ssh/x:enabled' --value 'true' \
+           --update '/x:ncs-config/x:netconf-north-bound/x:transport/x:ssh/x:enabled' --value 'true' \
+           --update '/x:ncs-config/x:netconf-call-home/x:enabled' --value 'true' \
            $CONF_FILE
 
 # conditionally enable webUI with no TLS on port 80
 if [ "$HTTP_ENABLE" == "true" ]; then
     xmlstarlet edit --inplace -N x=http://tail-f.com/yang/tailf-ncs-config \
-               --update "/x:ncs-config/x:webui/x:transport/x:tcp/x:enabled" --value "true" \
+               --update '/x:ncs-config/x:webui/x:transport/x:tcp/x:enabled' --value 'true' \
                $CONF_FILE
 fi
 
 # conditionally enable webUI with TLS on port 443
 if [ "$HTTPS_ENABLE" == "true" ]; then
     xmlstarlet edit --inplace -N x=http://tail-f.com/yang/tailf-ncs-config \
-               --update "/x:ncs-config/x:webui/x:transport/x:ssl/x:enabled" --value "true" \
-               --update "/x:ncs-config/x:webui/x:transport/x:ssl/x:key-file" --value "${NCS_RUN_DIR}/ssl/cert/host.key" \
-               --update "/x:ncs-config/x:webui/x:transport/x:ssl/x:cert-file" --value "${NCS_RUN_DIR}/ssl/cert/host.cert" \
+               --update '/x:ncs-config/x:webui/x:transport/x:ssl/x:enabled' --value 'true' \
+               --update '/x:ncs-config/x:webui/x:transport/x:ssl/x:key-file' --value '${NCS_RUN_DIR}/ssl/cert/host.key' \
+               --update '/x:ncs-config/x:webui/x:transport/x:ssl/x:cert-file' --value '${NCS_RUN_DIR}/ssl/cert/host.cert' \
                $CONF_FILE
 fi
