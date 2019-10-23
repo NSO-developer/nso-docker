480a18c9233a539ee5b687324e776266a0de9a5f
Kristian Larsson <kristian@spritelink.net>
2019-10-23 09:02:17 +0200

    Feed in command to ncs_cli the right way
    What happened? I've done this a thousand times, how could I forget?

--- test/test-post-start-script/test-post-start.sh
+++ test/test-post-start-script/test-post-start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # this will run a simple command against NCS demonstrating that NCS has
 # correctly started to its normal phase2 and is listening on commands
-ncs_cli -u admin -g ncsadmin -c "show version"
+echo "show ncs-state version " | ncs_cli -u admin -g ncsadmin
 if [ $? -eq 0 ]; then
     echo "post-start-script result: success"
 else
