909bcb81c1b589c8f4cea5319f1b732017d54634
Kristian Larsson <kristian@spritelink.net>
2020-05-10 15:34:25 +0200

    Fix testenv-build and include build-info
    The testenv-build target was simply broken. I'm really quite surprised how I
    must have committed broken code when I know that I tested all the cases and did
    it on the bgworker repo. Somehow I must have broken things when I transferred
    that code to the upstream skeletons. Anyway, it is fixed now.
    
    We now generate a build-meta-data.xml and inject into the package, if the
    package does not already have one. Very useful for operations!

--- skeletons/ned/nidned.mk
+++ skeletons/ned/nidned.mk
@@ -1,4 +1,4 @@
-# Common Makefile for NSO in Docker package standard form.
+# Common Makefile for NSO in Docker NED standard form.
 #
 # A repository that follows the standard form for a NID (NSO in Docker) package
 # repository contains one or more NSO packages in the `/packages` directory.
@@ -68,9 +68,9 @@ Dockerfile: Dockerfile.in $(wildcard includes/*)
 
 
 build: check-nid-available Dockerfile
-	docker build --target netsim -t $(IMAGE_PATH)$(PROJECT_NAME)/netsim:$(DOCKER_TAG) --build-arg NSO_IMAGE_PATH=$(NSO_IMAGE_PATH) --build-arg NSO_VERSION=$(NSO_VERSION) --build-arg NED_NAME=$(NED_NAME) .
-	docker build --target testnso -t $(IMAGE_PATH)$(PROJECT_NAME)/testnso:$(DOCKER_TAG) --build-arg NSO_IMAGE_PATH=$(NSO_IMAGE_PATH) --build-arg NSO_VERSION=$(NSO_VERSION) .
-	docker build --target package -t $(IMAGE_PATH)$(PROJECT_NAME)/package:$(DOCKER_TAG) --build-arg NSO_IMAGE_PATH=$(NSO_IMAGE_PATH) --build-arg NSO_VERSION=$(NSO_VERSION) .
+	docker build --target netsim -t $(IMAGE_PATH)$(PROJECT_NAME)/netsim:$(DOCKER_TAG) --build-arg NSO_IMAGE_PATH=$(NSO_IMAGE_PATH) --build-arg NSO_VERSION=$(NSO_VERSION) --build-arg NED_NAME=$(NED_NAME) --build-arg PKG_FILE=$(IMAGE_PATH)$(PROJECT_NAME)/package:$(DOCKER_TAG) .
+	docker build --target testnso -t $(IMAGE_PATH)$(PROJECT_NAME)/testnso:$(DOCKER_TAG) --build-arg NSO_IMAGE_PATH=$(NSO_IMAGE_PATH) --build-arg NSO_VERSION=$(NSO_VERSION) --build-arg PKG_FILE=$(IMAGE_PATH)$(PROJECT_NAME)/package:$(DOCKER_TAG) .
+	docker build --target package -t $(IMAGE_PATH)$(PROJECT_NAME)/package:$(DOCKER_TAG) --build-arg NSO_IMAGE_PATH=$(NSO_IMAGE_PATH) --build-arg NSO_VERSION=$(NSO_VERSION) --build-arg PKG_FILE=$(IMAGE_PATH)$(PROJECT_NAME)/package:$(DOCKER_TAG) .
 
 push:
 	docker push $(IMAGE_PATH)$(PROJECT_NAME)/package:$(DOCKER_TAG)
@@ -109,6 +109,10 @@ testenv-start:
 # package directory name, thus we use xmlstarlet to get the package name from
 # package-meta-data.xml. A full package reload can be forced by setting
 # PACKAGE_RELOAD to anything non-empty.
+#
+# build-meta-data.xml is also generated for packages that do not ship / build
+# one themselves. Note how NSO only reads in build-meta-data.xml on package
+# *reload*. A package *redeploy* will thus lead to a stale view in NSO.
 SUPPORTS_NEW_REDEPLOY=$(shell if [ $(NSO_VERSION_MAJOR) -gt 5 ] || [ $(NSO_VERSION_MAJOR) -eq 5 -a $(NSO_VERSION_MINOR) -ge 3 ]; then echo "true"; fi)
 ifeq ($(SUPPORTS_NEW_REDEPLOY),true)
 RELOAD_PATTERN="(package-meta-data.xml|\.cli$$|\.yang$$)"
@@ -119,12 +123,15 @@ testenv-build:
 	for NSO in $$(docker ps --format '{{.Names}}' --filter label=$(CNT_PREFIX) --filter label=nidtype=nso); do \
 		echo "-- Rebuilding for NSO: $${NSO}"; \
 		mkdir -p tmp && \
-		docker run -it --rm -v $(PWD):/src --volumes-from $${NSO} $(NSO_IMAGE_PATH)cisco-nso-dev:$(NSO_VERSION) bash -lc 'rsync -aEim /src/packages/. /src/test-packages/. /var/opt/ncs/packages/ > /src/tmp/rsync.log; chown $$(stat -c "%u:%g" /src/tmp) /src/tmp/rsync.log 2>/dev/null; for PKG in $$(find /src/packages /src/test-packages -mindepth 1 -maxdepth 1 -type d | xargs -n1 basename); do make -C /var/opt/ncs/packages/$${PKG}/src; done' && \
+		docker run -it --rm -v $(PWD):/src --volumes-from $${NSO} -e PKG_FILE=$(IMAGE_PATH)$(PROJECT_NAME)/package:$(DOCKER_TAG) $(NSO_IMAGE_PATH)cisco-nso-dev:$(NSO_VERSION) bash -lc 'rsync -aEim /src/packages/. /src/test-packages/. /var/opt/ncs/packages/ > /src/tmp/rsync.log; chown $$(stat -c "%u:%g" /src/tmp) /src/tmp/rsync.log 2>/dev/null; for PKG_DIR in $$(find /src/packages /src/test-packages -mindepth 1 -maxdepth 1 -type d); do export PKG_NAME=$$(basename $${PKG_DIR}); make -C /var/opt/ncs/packages/$${PKG_NAME}/src; OUTPUT_PATH=/var/opt/ncs/packages/$${PKG_NAME}/ make -f /src/nid/bmd.mk -C $${PKG_DIR} build-meta-data.xml; done' && \
 		egrep $(RELOAD_PATTERN) tmp/rsync.log >/dev/null; if [ $$? -eq 0 ] || [ -n "$$PACKAGE_RELOAD" ]; then \
+			echo "-- Reloading packages for NSO $${NSO}"; \
 			$(MAKE) testenv-runcmdJ CMD="request packages reload force"; \
 		else \
 			for PKG in $$(sed 's,^[^ ]\+ \([^/]\+\).*,\1,' tmp/rsync.log | sort | uniq); do \
-				PKG_NAME=$$(xmlstarlet sel -N x=http://tail-f.com/ns/ncs-packages -t -v "/x:ncs-package/x:name" -nl) \
+				echo "-- Redeploying package $${PKG} for NSO $${NSO}"; \
+				PMD_FILE=$$(ls packages/$${PKG}/package-meta-data.xml packages/$${PKG}/src/package-meta-data.xml.in test-packages/$${PKG}/package-meta-data.xml test-packages/$${PKG}/src/package-meta-data.xml.in 2>/dev/null | head -n1); \
+				PKG_NAME=$$(xmlstarlet sel -N x=http://tail-f.com/ns/ncs-packages -t -v "/x:ncs-package/x:name" -nl $${PMD_FILE}) && \
 				$(MAKE) testenv-runcmdJ CMD="request packages package $${PKG_NAME} redeploy"; \
 			done; \
 		fi; \
