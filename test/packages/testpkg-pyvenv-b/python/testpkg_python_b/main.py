93bd2e351468036204b978459aa5e1fa4870a106
Kristian Larsson <kristian@spritelink.net>
2020-09-04 11:50:17 +0200

    Add test packages for inter-package pyvenv dep test
    testpkg-pyvenv-a depends on foo that is in the virtualenv of testpkg-pyvenv-a,
    so if that imports correctly we can detect that the local package virtualenv is
    correctly loaded. testpkg-pyvenv-a also depends on testpkg-pyvenv-b, which in
    turn depends on package bar, which is in the virtualenv of testpkg-pyvenv-b.
    Thus, for testpkg-pyvenv-a to correctly load, we must support adding the
    virtualenv of another package to our path.
    
    testpkg-pyvenv-b is equivalent to testpkg-pyvenv, just renamed.

---
+++ test/packages/testpkg-pyvenv-b/python/testpkg_python_b/main.py
@@ -0,0 +1,21 @@
+# -*- mode: python; python-indent: 4 -*-
+import ncs
+from ncs.dp import Action
+
+import bar
+
+class TestAction(Action):
+    @Action.action
+    def cb_action(self, uinfo, name, kp, action_input, action_output):
+        action_output.message = "Hello world from Python pyvenv-b"
+
+
+class Main(ncs.application.Application):
+    def setup(self):
+        self.log.info('Main RUNNING')
+
+        self.register_action('test-pyvenv-b-actionpoint', TestAction)
+
+
+    def teardown(self):
+        self.log.info('Main FINISHED')
