7b447a175771cb537e3de3d3528a56874fa14ae2
Kristian Larsson <kristian@spritelink.net>
2021-04-22 17:56:14 +0200

    Add ability to restart NSO process
    Sending SIGHUP to the container, like with docker kill --signal=HUP ..., will
    now be handled by the init script in the container and restart NSO.
    
    In a pure containeresque environment, this is generally frowned upon. We should
    be restarting the container and not a process in the container. However, we have
    already implemented certain measures that are similarly non-containeresque and
    by allowing our users to opt-in to some, and not all, idiomatic container
    behavior on day 1, we enable a gradual transition from where users are today to
    a container native environment. That's a good thing overall and in the general
    direction of where we want to be.

--- docker-images/extra-files/run-nso.sh
+++ docker-images/extra-files/run-nso.sh
@@ -5,26 +5,57 @@ source /opt/ncs/installdirs
 export NCS_CONFIG_DIR NCS_LOG_DIR NCS_RUN_DIR
 
 # install signal handlers
+trap sighup_handler HUP
 trap sigint_handler INT
 trap sigquit_handler QUIT
 trap sigterm_handler TERM
 
+sighup_handler() {
+    echo "run-nso.sh: received SIGHUP, restarting NSO"
+    RESTART_REQUESTED=true
+    stop_nso
+    start_nso
+}
+
 sigint_handler() {
     echo "run-nso.sh: received SIGINT, stopping NSO"
-    ncs --stop
+    stop_nso
     exit 130 # 128+2
 }
 
 sigquit_handler() {
     echo "run-nso.sh: received SIGQUIT, stopping NSO"
-    ncs --stop
+    stop_nso
     exit 131 # 128+3
 }
 
 sigterm_handler() {
     echo "run-nso.sh: received SIGTERM, stopping NSO"
-    ncs --stop
+    stop_nso
     exit 143 # 128+15
+}
+
+# -- start NSO in the background
+# The 'set +-m' are for job control monitor mode. As job control monitor mode is
+# disabled per default, starting new processes places them in the same process
+# group as this script. When ctrl-c is pressed, SIGINT is delivered to all the
+# processes in the foreground process group, which would then include ncs. ncs
+# is really the Erlang BEAM VM, just renamed, and it doesn't handle ^c well - it
+# doesn't shut down ncs cleanly. To avoid this, we enable job control monitor
+# mode so that ncs is started as a background task in a different process group,
+# thus avoiding sending SIGINT to it on ^c. Instead we can handle SIGINT and
+# nicely ask ncs to shut down.
+start_nso() {
+    set -m
+    # output logs to stdout a la container style
+    ncs --cd ${NCS_RUN_DIR} -c ${NCS_CONFIG_DIR}/ncs.conf --foreground -v --with-package-reload-force &
+    NSO_PID="$!"
+    set +m
+}
+
+# Stop NSO
+stop_nso() {
+    ncs --stop
 }
 
 # Increase JAVA VM MAX Heap size to 4GB, also enable the new G1 GC in Java 8
@@ -193,35 +224,21 @@ for FILE in $(ls /etc/ncs/pre-ncs-start.d/*.sh 2>/dev/null); do
     ${FILE} || exit 1
 done
 
-# -- start NSO in the background
-# The 'set +-m' are for job control monitor mode. As job control monitor mode is
-# disabled per default, starting new processes places them in the same process
-# group as this script. When ctrl-c is pressed, SIGINT is delivered to all the
-# processes in the foreground process group, which would then include ncs. ncs
-# is really the Erlang BEAM VM, just renamed, and it doesn't handle ^c well - it
-# doesn't shut down ncs cleanly. To avoid this, we enable job control monitor
-# mode so that ncs is started as a background task in a different process group,
-# thus avoiding sending SIGINT to it on ^c. Instead we can handle SIGINT and
-# nicely ask ncs to shut down.
-set -m
-# output logs to stdout a la container style
-ncs --cd ${NCS_RUN_DIR} -c ${NCS_CONFIG_DIR}/ncs.conf --foreground -v --with-package-reload-force &
-NSO_PID="$!"
-set +m
+start_nso
 
 # sleep a bit so ncs has a chance to start up IPC port etc
 sleep 3
 # Wait for NSO to start by continuously ensuring the NSO PID is alive, i.e. that
-# NSO hasn't and NSO reports having started up. If CDB is corrupt or there are
-# other similar problems during startup, ncs --wait-started will hang, since it
-# is waiting for NSO. If NSO has died, there is no point waiting, thus we also
-# check for the NSO PID being alive.
+# NSO hasn't exited and NSO reports having started up. If CDB is corrupt or
+# there are other similar problems during startup, ncs --wait-started will hang,
+# since it is waiting for NSO. If NSO has died, there is no point waiting, thus
+# we also check for the NSO PID being alive.
 echo "Waiting for NSO to start..."
 for I in $(seq 60); do
     if ! kill -s 0 ${NSO_PID} >/dev/null 2>&1; then
         wait ${NSO_PID}
         EXIT_CODE=$?
-        echo "run-nso.sh: NSO is dead (exit code ${EXIT_CODE}) - exiting container"
+        echo "run-nso.sh: NSO exited during startup (exit code ${EXIT_CODE}) - exiting container"
         exit ${EXIT_CODE}
     fi
     ncs --wait-started 10 && break
@@ -234,9 +251,19 @@ for FILE in $(ls /etc/ncs/post-ncs-start.d/*.sh 2>/dev/null); do
 done
 
 # wait forever on the ncs process, we run ncs in background and wait on it like
-# this, with a signal handler for INT & TERM so that we upon receiving those
-# signals can run ncs --stop rather than having those signals sent raw to ncs
-wait ${NSO_PID}
-EXIT_CODE=$?
-echo "run-nso.sh: NSO exited (exit code ${EXIT_CODE}) - exiting container"
-exit ${EXIT_CODE}
+# this, with a signal handler for HUP, INT & TERM so that we upon receiving
+# those signals can run ncs --stop rather than having those signals sent
+# directly to ncs. For HUP we set RESTART_REQUEST=true and will take another
+# spin in our loop.
+while true; do
+    echo "run-nso.sh: waiting for signals or NSO (PID ${NSO_PID}) to exit..."
+    RESTART_REQUESTED=false
+    wait ${NSO_PID}
+    EXIT_CODE=$?
+    if [ "${RESTART_REQUESTED}" = true ]; then
+        echo "run-nso.sh: NSO exited (exit code ${EXIT_CODE}) - NSO restarting..."
+    else
+        echo "run-nso.sh: NSO exited (exit code ${EXIT_CODE}) - exiting container"
+        exit ${EXIT_CODE}
+    fi
+done
